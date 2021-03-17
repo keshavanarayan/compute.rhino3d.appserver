0c458e5213b7dcf9dcb574a3e8af16b765d9622c
keshavanarayan <74804559+keshavanarayan@users.noreply.github.com>
2021-03-17 13:21:23 +0530

    bg color

--- src/examples/RandomContextGenerator/script.js
+++ src/examples/RandomContextGenerator/script.js
@@ -366,11 +366,11 @@ function init()
   THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
 
   scene = new THREE.Scene()
-  scene.background = new THREE.Color(0xff0000, 1)
+  scene.background = new THREE.Color("rgb(255, 255, 255)")
   camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 1, 10000 )
-  camera.position.x = 1000
-  camera.position.y = 1000
-  camera.position.z = 1000
+  camera.position.x = 100
+  camera.position.y = 100
+  camera.position.z = 100
 
   renderer = new THREE.WebGLRenderer({antialias: true})
   renderer.setPixelRatio( window.devicePixelRatio )
